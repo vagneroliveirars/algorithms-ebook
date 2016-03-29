a60daf6cb11207ad241b4db3e0a55ef136607b07
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-03-29 09:38:29 -0300

    Added new classes

---
+++ src/chapter1/question3/DuplicateCharacters.java
@@ -0,0 +1,48 @@
+package chapter1.question3;
+
+public class DuplicateCharacters {
+
+	public static void main(String[] args) {
+		String s = "abcd";
+		
+		char[] str = s.toCharArray();
+		removeDuplicates(str);
+	}
+
+	public static void removeDuplicates(char[] str) {
+
+		if (str == null)
+			return;
+
+		int len = str.length;
+
+		if (len < 2)
+			return;
+
+		int tail = 1;
+
+		for (int i = 1; i < len; ++i) {
+
+			int j;
+
+			for (j = 0; j < tail; ++j) {
+
+				if (str[i] == str[j])
+					break;
+
+			}
+
+			if (j == tail) {
+
+				str[tail] = str[i];
+
+				++tail;
+
+			}
+
+		}
+
+		str[tail] = 0;
+	}
+
+}
